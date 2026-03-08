ec9117d3422eceb871428b205d49b86ba8181bcb
stanislav-commits <stanislav@trident-virtual.com>
2026-03-08 16:54:03 +0200

    feat(metrics): wire influx module and sync scheduler

--- backend/src/metrics/metrics.module.ts
+++ backend/src/metrics/metrics.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { InfluxdbModule } from '../influxdb/influxdb.module';
+import { MetricsSyncScheduler } from './metrics-sync.scheduler';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
 
 @Module({
+  imports: [InfluxdbModule],
   controllers: [MetricsController],
-  providers: [MetricsService],
+  providers: [MetricsService, MetricsSyncScheduler],
   exports: [MetricsService],
 })
 export class MetricsModule {}
