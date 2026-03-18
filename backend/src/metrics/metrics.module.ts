40b35e99f267f27758d5865bf2c89c175006a50f
stanislav-commits <stanislav@trident-virtual.com>
2026-03-18 23:38:35 +0200

    Add metric description generator service

--- backend/src/metrics/metrics.module.ts
+++ backend/src/metrics/metrics.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { InfluxdbModule } from '../influxdb/influxdb.module';
+import { MetricDescriptionService } from './metric-description.service';
 import { MetricsSyncScheduler } from './metrics-sync.scheduler';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
@@ -7,7 +8,7 @@ import { MetricsService } from './metrics.service';
 @Module({
   imports: [InfluxdbModule],
   controllers: [MetricsController],
-  providers: [MetricsService, MetricsSyncScheduler],
+  providers: [MetricsService, MetricDescriptionService, MetricsSyncScheduler],
   exports: [MetricsService],
 })
 export class MetricsModule {}
