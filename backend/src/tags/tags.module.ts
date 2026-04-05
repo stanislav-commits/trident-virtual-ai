0ec85fd4c25e1ebe2e6c7daa35c2465ecc76c9b1
stanislav-commits <stanislav@trident-virtual.com>
2026-04-05 21:54:36 +0300

    Update backend/src/tags/tags.module.ts

--- backend/src/tags/tags.module.ts
+++ backend/src/tags/tags.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { RagflowModule } from '../ragflow/ragflow.module';
 import { TagsController } from './tags.controller';
 import { TagsService } from './tags.service';
+import { TagLinksService } from './tag-links.service';
+import { TagMatcherService } from './tag-matcher.service';
 
 @Module({
+  imports: [RagflowModule],
   controllers: [TagsController],
-  providers: [TagsService],
-  exports: [TagsService],
+  providers: [TagsService, TagMatcherService, TagLinksService],
+  exports: [TagsService, TagMatcherService, TagLinksService],
 })
 export class TagsModule {}
