760938738f9fbe1c2d7ca0951fe22c2f5d3b32d1
stanislav-commits <stanislav@trident-virtual.com>
2026-04-05 21:54:33 +0300

    Update backend/src/metrics/metrics.module.ts

--- backend/src/metrics/metrics.module.ts
+++ backend/src/metrics/metrics.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GrafanaLlmModule } from '../grafana-llm/grafana-llm.module';
 import { InfluxdbModule } from '../influxdb/influxdb.module';
+import { TagsModule } from '../tags/tags.module';
 import { MetricDescriptionService } from './metric-description.service';
 import { MetricsSyncScheduler } from './metrics-sync.scheduler';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
 
 @Module({
-  imports: [InfluxdbModule, GrafanaLlmModule],
+  imports: [InfluxdbModule, GrafanaLlmModule, TagsModule],
   controllers: [MetricsController],
   providers: [MetricsService, MetricDescriptionService, MetricsSyncScheduler],
   exports: [MetricsService],
